ec6c5c2b25dfa2e7755f8f71ff8a4d405c563228
Jaylon Gowie <jaylongowie46@gmail.com>
2021-09-10 23:07:23 -0500

    Let's fix this script real quick

--- renpy-vita/link_compile_pylibs.bash
+++ renpy-vita/link_compile_pylibs.bash
@@ -1,6 +1,5 @@
 mkdir -p DistributionTool/build/lib
 mkdir -p tmp_build
-rm python27.zip
 
 rm -rf tmp_build/
 cp -as $VITASDK/arm-vita-eabi/lib/python2.7 tmp_build
@@ -8,7 +7,7 @@ cp -as $(realpath ../pygame_sdl2/src/pygame_sdl2) tmp_build
 cp -as $(realpath ../renpy/renpy) tmp_build
 rm -r tmp_build/test
 rm -r tmp_build/lib2to3/tests
-rm -r tmp_build/test
+rm -r tmp_build/test # You need to do this btw. I have no idea why. Maybe Julian does. I don't do bash. lol
 rm -r tmp_build/youtube_dl
 pushd tmp_build
 find . -type l -not -name \*.py -delete
@@ -18,7 +17,7 @@ find . -type l -not -name \*.pyc -delete
 7z a -tzip ../python27.zip .
 popd
 rm -rf DistributionTool/build/lib/python27.zip
-cp python27.zip DistributionTool/build/lib/python27.zip
+mv python27.zip DistributionTool/build/lib/python27.zip
 
 rm -rf DistributionTool/build/renpy
 mkdir -p DistributionTool/build/renpy
